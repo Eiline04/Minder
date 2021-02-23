3500ede5475cc446a56dda82630f3c939b493d05
Eiline04 <tinitza04@gmail.com>
2021-02-23 23:19:59 +0200

    Created the first quiz template - True or False. It works good, but needs improvement.

--- app/src/main/java/com/technovation/sagetech/minder/quizzez/Set1AdevaratFals.java
+++ app/src/main/java/com/technovation/sagetech/minder/quizzez/Set1AdevaratFals.java
@@ -1,17 +1,23 @@
 package com.technovation.sagetech.minder.quizzez;
+import com.technovation.sagetech.minder.quizzez.Test1;
+
 import java.util.Random;
 
-public class Set1AdevaratFals extends Test1 {
-
+/*public class Set1AdevaratFals  {
+  public int score = 0;
+    public int questionNumber = 1;
+    protected String[] questionDataArray;
+    boolean isQuestionLeft;
 // private int wrong_answers, correct_answers;
- private int[] no_questions_ToAsk ={0,0,0,0,0};
+ public int[] no_questions_ToAsk ={-1,-1,-1,-1,-1};
+ Test1 test1varr;
 
 
     //---------------------THE FUNCTION THAT GETS THE QUESTION--------------------------
     public void setQuestion(){
         isQuestionLeft = false;
         for(int i = 0; i<5 && !isQuestionLeft; i++){
-            if(no_questions_ToAsk[i] == 0) isQuestionLeft = true;
+            if(no_questions_ToAsk[i] == -1) isQuestionLeft = true;
         }
 
         Random rand = new Random();
@@ -22,41 +28,41 @@ public class Set1AdevaratFals extends Test1 {
             if(no_questions_ToAsk[i] == randomNo) questionAlreadyAsked = true;
         }
 
-        if(no_questions_ToAsk[questionNumber-1] == 0 && !questionAlreadyAsked && isQuestionLeft){
+        if(no_questions_ToAsk[questionNumber-1] == -1 && !questionAlreadyAsked && isQuestionLeft){
             no_questions_ToAsk[questionNumber-1] = randomNo;
             switch (randomNo){
                 case 0:
-                     questionDataArray = getQuestionData("Se traverseaza pe culoarea verde a semaforului.","true");
+                     questionDataArray = test1varr.getQuestionData("Se traverseaza pe culoarea verde a semaforului.","true");
                     break;
                 case 1:
-                    questionDataArray = getQuestionData("Marul este o leguma.","false");
+                    questionDataArray = test1varr.getQuestionData("Marul este o leguma.","false");
                     break;
                 case 2:
-                    questionDataArray = getQuestionData("Daca ploua, este bine sa folosim umbrela.", "true");
+                    questionDataArray = test1varr.getQuestionData("Daca ploua, este bine sa folosim umbrela.", "true");
                     break;
                 case 3:
-                    questionDataArray = getQuestionData("Catelul are patru picioare.", "true");
+                    questionDataArray = test1varr.getQuestionData("Catelul are patru picioare.", "true");
                     break;
                 case 4:
-                    questionDataArray = getQuestionData("Soarele este vizibil noaptea.","false");
+                    questionDataArray = test1varr.getQuestionData("Soarele este vizibil noaptea.","false");
                     break;
                 case 5:
-                    questionDataArray = getQuestionData("Apa are gust.","false");
+                    questionDataArray = test1varr.getQuestionData("Apa are gust.","false");
                     break;
                 case 6:
-                    questionDataArray = getQuestionData("Apa de mare este sarata.","true");
+                    questionDataArray = test1varr.getQuestionData("Apa de mare este sarata.","true");
                     break;
                 case 7:
-                    questionDataArray = getQuestionData("Lamaia este acra.","true");
+                    questionDataArray = test1varr.getQuestionData("Lamaia este acra.","true");
                     break;
                 case 8:
-                    questionDataArray = getQuestionData("Ciresii infloresc vara.","false");
+                    questionDataArray = test1varr.getQuestionData("Ciresii infloresc vara.","false");
                     break;
                 default:
-                    questionDataArray = getQuestionData("Pinguinii zboara.","false");
+                   questionDataArray = test1varr.getQuestionData("Pinguinii zboara.","false");
             }
         } else if(isQuestionLeft) setQuestion();
     }
 
 }
-
+*/
--- app/src/main/java/com/technovation/sagetech/minder/quizzez/Test1.java
+++ app/src/main/java/com/technovation/sagetech/minder/quizzez/Test1.java
@@ -10,69 +10,142 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.technovation.sagetech.minder.R;
+
+import java.util.Random;
+
 //--------------THIS CLASS SHOULD START AFTER START BUTTON FROM MainActivity IS PRESSED------------------
 public class Test1 extends AppCompatActivity {
 
     public int score = 0;
-    public int questionNumber = 1;
-    protected boolean finish = false;
-    String[] questionDataArray;
-    boolean isQuestionLeft, buttonPressed;
-    Set1AdevaratFals afVarr;
-    TextView questionTextView;
-    Button trueBtn, falseBtn;
+    private int questionNumber = 1;
+    private final int[] no_questions_ToAsk ={-1,-1,-1,-1,-1};
+    private String[] questionDataArray;
+    private boolean isQuestionLeft;
+    private TextView questionTextView, questionNumberTextView;
+    private Button trueBtn, falseBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.adevarat_sau_fals);
 
         questionTextView = findViewById(R.id.intrebareTextView_AsauF);
+        questionNumberTextView = findViewById(R.id.numarIntrebare_AsauF);
         trueBtn = findViewById(R.id.trueBtn);
         falseBtn = findViewById(R.id.falseBtn);
 
         questionDataArray = new String[2];
-        isQuestionLeft = true;
-        buttonPressed = false;
 
-        while(isQuestionLeft){
-            buttonPressed = false;
-            afVarr.setQuestion();
-            questionTextView.setText(afVarr.questionDataArray[0]);
+        setQuestion();
+        questionTextView.setText(questionDataArray[0]);
+        questionNumberTextView.setText(String.valueOf(questionNumber));
 
-            while(!buttonPressed){
-                trueBtn.setOnClickListener(v -> {
-                    if(afVarr.questionDataArray[1].equals("true")) {
-                        score += 10;
-                        Toast.makeText(Test1.this,"Corect!",Toast.LENGTH_SHORT).show();
-                    }else {
-                        Toast.makeText(Test1.this,"Gresit!",Toast.LENGTH_SHORT).show();
-                    }
-                    buttonPressed = true;
-                });
+        //------------Click Listener for the TRUE button
+        trueBtn.setOnClickListener(v -> {
+            if (isQuestionLeft) {
+                if (questionDataArray[1].equals("true")) {
+                    score += 10;
+                    Toast.makeText(Test1.this, "Corect!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Test1.this, "Gresit!", Toast.LENGTH_SHORT).show();
+                }
+                questionNumber++;
+                if (isQuestionLeft) {
+                    setQuestion();
+                    questionTextView.setText(questionDataArray[0]);
+                    questionNumberTextView.setText(String.valueOf(questionNumber));
+                }
+            } else Toast.makeText(Test1.this, "Testul s-a terminat!", Toast.LENGTH_SHORT).show();
+        });
 
-                falseBtn.setOnClickListener(v -> {
-                    if(afVarr.questionDataArray[1].equals("false")) {
-                        score += 10;
-                        Toast.makeText(Test1.this,"Corect!",Toast.LENGTH_SHORT).show();
-                    }else {
-                        Toast.makeText(Test1.this,"Gresit!",Toast.LENGTH_SHORT).show();
-                    }
-                    buttonPressed = true;
-                });
 
-            }
-            questionNumber++;
-        }
-        Toast.makeText(Test1.this,"Testul s-a terminat!",Toast.LENGTH_SHORT).show();
+        //------------Click Listener for the FALSE button
+        falseBtn.setOnClickListener(v -> {
+            if (isQuestionLeft) {
+                if (questionDataArray[1].equals("false") && isQuestionLeft) {
+                    score += 10;
+                    Toast.makeText(Test1.this, "Corect!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Test1.this, "Gresit!", Toast.LENGTH_SHORT).show();
+                }
+                questionNumber++;
+                if (isQuestionLeft) {
+                    setQuestion();
+                    questionTextView.setText(questionDataArray[0]);
+                    questionNumberTextView.setText(String.valueOf(questionNumber));
+                }
+            } else Toast.makeText(Test1.this, "Testul s-a terminat!", Toast.LENGTH_SHORT).show();
+        });
+
+
     }
 
+    //--------------------CREATING A ARRAY WITH THE QUESTION AND ITS ANSWER-----------------
     String[] getQuestionData(String question, String answer){
-            String[] questionAndAnswer = new String[2];
-            questionAndAnswer[0] = question;
-            questionAndAnswer[1] = answer;
-            return questionAndAnswer;
+        String[] questionAndAnswer = new String[2];
+        questionAndAnswer[0] = question;
+        questionAndAnswer[1] = answer;
+        return questionAndAnswer;
     }
+
+    //--------------------------THE FUNCTION THAT GETS THE QUESTION--------------------------
+    public void setQuestion(){
+        if(questionNumber<=6) {
+            isQuestionLeft = false;
+
+            //Verifying if there is a question remained to ask
+            for (int i = 0; i < 5 && !isQuestionLeft; i++) {
+                if (no_questions_ToAsk[i] == -1) isQuestionLeft = true;
+            }
+
+
+            if (isQuestionLeft) {
+                Random rand = new Random();
+                int randomNo = rand.nextInt(9);
+
+                for (int i = 0; i < 5; i++) {
+                    if (no_questions_ToAsk[i] == randomNo) setQuestion();
+                }
+
+                if (no_questions_ToAsk[questionNumber - 1] == -1 && questionNumber<= 6) {
+                    no_questions_ToAsk[questionNumber - 1] = randomNo;
+                    switch (randomNo) {
+                        case 0:
+                            questionDataArray = getQuestionData("Se traverseaza pe culoarea verde a semaforului.", "true");
+                            break;
+                        case 1:
+                            questionDataArray = getQuestionData("Marul este o leguma.", "false");
+                            break;
+                        case 2:
+                            questionDataArray = getQuestionData("Daca ploua, este bine sa folosim umbrela.", "true");
+                            break;
+                        case 3:
+                            questionDataArray = getQuestionData("Catelul are patru picioare.", "true");
+                            break;
+                        case 4:
+                            questionDataArray = getQuestionData("Soarele este vizibil noaptea.", "false");
+                            break;
+                        case 5:
+                            questionDataArray = getQuestionData("Apa are gust.", "false");
+                            break;
+                        case 6:
+                            questionDataArray = getQuestionData("Apa de mare este sarata.", "true");
+                            break;
+                        case 7:
+                            questionDataArray = getQuestionData("Lamaia este acra.", "true");
+                            break;
+                        case 8:
+                            questionDataArray = getQuestionData("Ciresii infloresc vara.", "false");
+                            break;
+                        default:
+                            questionDataArray = getQuestionData("Pinguinii zboara.", "false");
+                    }
+                } else setQuestion();
+            }
+        }else Toast.makeText(Test1.this,"Sa trecem la urmatoarele intrebari",Toast.LENGTH_SHORT).show();
+    }
+
+
 
 }
