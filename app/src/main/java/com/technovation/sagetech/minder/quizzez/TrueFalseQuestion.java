1099e69a2acd3040d301df4f783e37b4153ed40c
Eiline04 <tinitza04@gmail.com>
2021-03-25 20:59:48 +0200

    Fixed the Test1_TrueFalse java class.

---
+++ app/src/main/java/com/technovation/sagetech/minder/quizzez/TrueFalseQuestion.java
@@ -0,0 +1,35 @@
+package com.technovation.sagetech.minder.quizzez;
+
+public class TrueFalseQuestion implements Question {
+
+    private String question;
+    private Boolean answer;
+
+
+    public TrueFalseQuestion(String question, Boolean answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    @Override
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Boolean getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Boolean answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public Boolean isCorrect(Object givenAnswer) {
+        return answer == (Boolean) givenAnswer;
+    }
+}
